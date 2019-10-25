fbee39b343024970fbb1935e79b2312a92d5e4f3
sh4y <akshay.hariharan@mail.utoronto.ca>
2019-10-25 11:04:42 +0200

    Create pyscript.py

---
+++ pyscript.py
@@ -0,0 +1,12 @@
+import string;
+import random;
+
+letters = list(string.ascii_lowercase)
+f = 'C:/testfile.txt'
+with open(f, mode='w') as f:
+    for x in range(100):
+        f.write(random.choice(letters))
+
+f.write('\n')
+f.close()
+
